bba716b89044eefb3850a94ed356a4b1ce918ac6
helpfulrobot <chris@silverstripe.com>
2015-12-20 17:32:27 +1300

    Converted to PSR-2

--- code/SitemapPage.php
+++ code/SitemapPage.php
@@ -5,20 +5,22 @@
  * 
  * @package sitemap2
  */
-class SitemapPage extends Page {
-	
-	private static $icon = 'sitemap2/images/sitemap.png';
-	
-	private static $description = 'Page displaying the sitemap for this website as a nested tree';
-	
-	/**
-	 * Retrieves the root pages to be used in the sitemap
-	 * 
-	 * @return DataList
-	 */
-	public function SitemapRootPages() {
-		return $this->SitemapChildrenOfParent(0);
-	}
+class SitemapPage extends Page
+{
+    
+    private static $icon = 'sitemap2/images/sitemap.png';
+    
+    private static $description = 'Page displaying the sitemap for this website as a nested tree';
+    
+    /**
+     * Retrieves the root pages to be used in the sitemap
+     * 
+     * @return DataList
+     */
+    public function SitemapRootPages()
+    {
+        return $this->SitemapChildrenOfParent(0);
+    }
 }
 
 /**
@@ -26,6 +28,6 @@ class SitemapPage extends Page {
  * 
  * @package sitemap2
  */
-class SitemapPage_Controller extends Page_Controller {
-	
+class SitemapPage_Controller extends Page_Controller
+{
 }
