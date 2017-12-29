1a1a9b5a2a2040ac5f593616eb5fb119ef4104bc
Hayden Shaw <haydens@e2digital.nz>
2017-12-29 15:51:11 +1300

    SS4 Upgrade
    * Added namespaces
    * Updated directory structures
    * Changed to vendor module

---
+++ src/SitemapPageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tractorcow\Sitemap2;
+
+use PageController;
+
+/**
+* @author Damian Mooyman
+*
+* @package sitemap2
+*/
+class SitemapPage_Controller extends PageController
+{
+
+}
